f53f5f2a8c6792ad7e8e4ebe947ebfca6a320443
admin <ltwig@outlook.com>
2021-07-06 19:02:37 +0300

    first commit

--- framework/api/rest_client.py
+++ framework/api/rest_client.py
@@ -15,20 +15,20 @@ class RestClient:
             f"\"GET\": {url}\nBad Response: {response} "
         return response
 
-    def post(self, path, basic_auth, content_type, payload, expected_response_status_code: int = HTTPStatus.OK):
-        url = self.base_url + path
-        request_body = json.dumps(payload)
-        response = requests.post(url, data=request_body, auth=basic_auth, headers=content_type)
-        assert response.status_code == expected_response_status_code, \
-            f"\"POST\": {url}\nBad Response: {response}"
-        return response
-
     def patch(self, path, basic_auth, content_type, payload, expected_response_status_code: int = HTTPStatus.OK):
         url = self.base_url + path
         request_body = json.dumps(payload)
         response = requests.patch(url, data=request_body, auth=basic_auth, headers=content_type)
         assert response.status_code == expected_response_status_code, \
             f"\"PATCH\": {url}\nBad Response: {response}"
+        return response
+    
+    def post(self, path, basic_auth, content_type, payload, expected_response_status_code: int = HTTPStatus.OK):
+        url = self.base_url + path
+        request_body = json.dumps(payload)
+        response = requests.post(url, data=request_body, auth=basic_auth, headers=content_type)
+        assert response.status_code == expected_response_status_code, \
+            f"\"POST\": {url}\nBad Response: {response}"
         return response
 
     def delete(self, path, basic_auth, content_type, expected_response_status_code: int = HTTPStatus.OK):
